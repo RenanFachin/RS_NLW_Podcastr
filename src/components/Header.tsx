33c8eb3bb4e55a50bec454339b05a15fa44c5bc6
Renan <renan.fachin@hotmail.com>
2023-01-26 18:09:39 -0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import format from "date-fns/format"
+import ptBR from "date-fns/locale/pt-BR"
+
+export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR
+    })
+
+    return (
+        <header className="bg-white h-28 flex items-center py-8 px-16 border border-solid border-gray-100">
+            <img
+                src="/logo.svg"
+                alt=""
+            />
+
+            <p className="ml-8 py-1 pl-8 border-l border-l-gray-100">
+                O melhor para você ouvir, sempre
+            </p>
+
+            <span className="ml-auto capitalize">
+                {currentDate}
+            </span>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import { Header } from "@/components/Header"
+
 export default function Home() {
   return (
     <div className="w-full h-screen ">
-      <h1 className="">
-        Configurado tailwindcss e nextjs
-      </h1>
+      <Header />
     </div>
   )
 }
