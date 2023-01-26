c7595ea75279fd35a5b51be50b8a8b21d3a14f60
Renan <renan.fachin@hotmail.com>
2023-01-26 15:21:53 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-slate-800">
+      <h1 className="text-white text-2xl">
+        Configurado tailwindcss e nextjs
+      </h1>
+    </div>
+  )
+}
